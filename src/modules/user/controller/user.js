33f9662f0048523cdb66f09557b8f98cd39aaabd
arifin kurnia <kurniaarifin27@gmail.com>
2022-08-05 17:12:59 +0700

    add create user & include profile on find all user

--- src/modules/user/controller/user.js
+++ src/modules/user/controller/user.js
@@ -2,13 +2,36 @@ const { prisma } = require('../../../../prisma');
 
 const findAll = async (req, res) => {
 	try {
-		const users = await prisma.user.findMany();
+		const users = await prisma.user.findMany({
+			include: {
+				profile: true
+			}
+		});
 		res.json({ data: users });
 	} catch (error) {
 		res.json({ message: error });
 	}
 };
 
+const create = async (req, res) => {
+	const { name, email, bio } = req.body;
+	try {
+		const user = await prisma.user.create({
+			data: {
+				name,
+				email,
+				profile: {
+					create: { bio }
+				}
+			}
+		});
+		res.json({ data: user });
+	} catch (error) {
+		res.json({ message: error });
+	}
+};
+
 module.exports = {
-	findAll
+	findAll,
+	create
 };
