1ab1f0fe6aa32119aa903de89a1d22722d96f447
arifin kurnia <kurniaarifin27@gmail.com>
2022-08-05 17:13:33 +0700

    add user validation on user route

--- src/modules/user/routes/user.js
+++ src/modules/user/routes/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { userController } = require('../controller')
+const { userController } = require('../controller');
+const { validate } = require('../../../middleware');
+const { userValidation } = require('../validation');
 
 const router = express.Router();
 
 router.get('/users', userController.findAll);
+router.post('/users', validate(userValidation.create), userController.create);
 
 module.exports = router;
