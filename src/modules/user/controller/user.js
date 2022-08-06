9a8ae43d93d674404c8d8e4128b56dbea9915a9e
arifin kurnia <kurniaarifin27@gmail.com>
2022-08-06 17:25:44 +0700

    add update user & refactor create user

--- src/modules/user/controller/user.js
+++ src/modules/user/controller/user.js
@@ -1,37 +1,97 @@
 const { prisma } = require('../../../../prisma');
+const httpStatus = require('http-status');
 
 const findAll = async (req, res) => {
 	try {
+		// find user(s) with profile
 		const users = await prisma.user.findMany({
 			include: {
 				profile: true
 			}
 		});
-		res.json({ data: users });
+		res.json({ code: httpStatus.OK, data: users });
 	} catch (error) {
-		res.json({ message: error });
+		res.json({ code: httpStatus.INTERNAL_SERVER_ERROR, message: error });
 	}
 };
 
 const create = async (req, res) => {
 	const { name, email, bio } = req.body;
 	try {
-		const user = await prisma.user.create({
-			data: {
-				name,
-				email,
-				profile: {
-					create: { bio }
+		// create user & user profile
+		const user = await prisma.user
+			.create({
+				data: {
+					name,
+					email,
+					profile: {
+						create: { bio }
+					}
 				}
-			}
-		});
-		res.json({ data: user });
+			})
+			.then(async (result) => {
+				if (result) {
+					return await prisma.user.findFirst({
+						where: {
+							id: result.id
+						},
+						include: {
+							profile: true
+						}
+					});
+				}
+			});
+		res.json({ code: httpStatus.OK, data: user });
 	} catch (error) {
-		res.json({ message: error });
+		res.json({ code: httpStatus.INTERNAL_SERVER_ERROR, message: error });
+	}
+};
+
+const update = async (req, res) => {
+	const { userId } = req.params;
+	const { name, email, bio } = req.body;
+	try {
+		const user = await prisma.user
+			.update({
+				where: {
+					id: Number(userId)
+				},
+				data: {
+					name,
+					email
+				}
+			})
+			.then(async (result) => {
+				// update user profile:bio if success updating user && request body:bio exist
+				if (result) {
+					if (bio)
+						await prisma.profile.update({
+							where: {
+								userId: Number(userId)
+							},
+							data: {
+								bio
+							}
+						});
+					// return updated user with profile
+					return await prisma.user.findUnique({
+						where: {
+							id: Number(userId)
+						},
+						include: {
+							profile: true
+						}
+					});
+				}
+			});
+		res.json({ code: httpStatus.OK, data: user });
+	} catch (error) {
+		res.json({ code: httpStatus.INTERNAL_SERVER_ERROR, message: error });
 	}
 };
 
 module.exports = {
 	findAll,
-	create
+	create,
+	update
 };
