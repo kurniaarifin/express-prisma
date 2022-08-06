9f207be34bc30b249c8e5f6f34fafe29ad22eb16
arifin kurnia <kurniaarifin27@gmail.com>
2022-08-06 17:26:11 +0700

    add update user route

--- src/modules/user/routes/user.js
+++ src/modules/user/routes/user.js
@@ -5,7 +5,11 @@ const { userValidation } = require('../validation');
 
 const router = express.Router();
 
+// get user(s)
 router.get('/users', userController.findAll);
+// create user(s)
 router.post('/users', validate(userValidation.create), userController.create);
+// updating user by id
+router.put('/users/:userId', validate(userValidation.update), userController.update);
 
 module.exports = router;
