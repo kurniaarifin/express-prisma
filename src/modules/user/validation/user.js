f4184a25f587176a76745733c14f65b1e8f99fff
arifin kurnia <kurniaarifin27@gmail.com>
2022-08-06 17:25:01 +0700

    add update user validation

--- src/modules/user/validation/user.js
+++ src/modules/user/validation/user.js
@@ -1,14 +1,28 @@
 const Joi = require('joi');
 
+// validate request body create new user
 const create = {
 	body: Joi.object()
 		.keys({
 			name: Joi.string().required(),
-			email: Joi.string().required().email()
+			email: Joi.string().required().email(),
+			bio: Joi.string().optional()
+		})
+		.unknown(false)
+};
+
+// validate request body on updating user
+const update = {
+	body: Joi.object()
+		.keys({
+			name: Joi.string().optional(),
+			email: Joi.string().optional().email(),
+			bio: Joi.string().optional()
 		})
 		.unknown(false)
 };
 
 module.exports = {
-	create
+	create,
+	update
 };
