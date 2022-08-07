4db6f120262e839c3ab5a9e6b9ff1d7735076bd8
arifin kurnia <kurniaarifin27@gmail.com>
2022-08-07 17:08:37 +0700

    add user test

--- test/user.test.js
+++ test/user.test.js
@@ -2,11 +2,31 @@ const request = require('supertest');
 
 const app = require('../index');
 
-describe('Health check!', () => {
-	test('GET /', (done) => {
-		request(app).get('/').expect('Content-Type', /json/).expect(200).end((err, res) => {
-			if (err) return done(err);
-			return done();
-		});
+describe('User api test', () => {
+	test('GET /users', (done) => {
+		request(app)
+			.get('/users')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.expect((res) => {
+				expect(Array.isArray(res.body.data)).toBe(true);
+			})
+			.end((err, res) => {
+				if (err) return done(err);
+				return done();
+			});
+	});
+	test('GET /users/:userId', (done) => {
+		request(app)
+			.get('/users/7')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.expect((res) => {
+				expect(typeof res.body.data === 'object' && res.body.data !== null).toBe(true);
+			})
+			.end((err, res) => {
+				if (err) return done(err);
+				return done();
+			});
 	});
 });
